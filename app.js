d24620db868634de9df0e92e9b489a27f39ecbe8
Ben Cox <benjamin.cox@uk.ibm.com>
2015-07-24 17:53:05 +0100

    Allow number, gender and country to be specified as arguments.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var http = require('http')
 var request = require('request')
+var bodyParser = require('body-parser')
+var propertiesParser = require('properties-parser')
 
 var host = process.env.VCAP_APP_HOST || 'localhost'
 var port = process.env.VCAP_APP_PORT || 1337
@@ -7,18 +9,61 @@ var port = process.env.VCAP_APP_PORT || 1337
 var express = require('express')
 var app = express()
 
-app.post('/slack', function(req, res) {
+app.post('/slack', bodyParser.text(), function(req, res) {
 	res.setHeader('Content-Type', 'text/plain')
 
+	var text = propertiesParser.parse(req.body).text
+	var qs = {}
+	if (text) {
+		var args = text.split(/\s+/)
+
+		if (args.length > 0) {
+			if (!isNaN(args[0])) {
+				qs.amount = +args.shift()
+			}
+		}
+		
+		if (args.length > 0) {
+			if (args[0] === "male" || args[0] === "female") {
+				qs.gender = args.shift()
+			}
+		}
+		
+		if (args.length > 0) {
+			qs.country = args.join(" ")
+		}
+
+	}
+
+	console.log("Requesting names from uinames.com. Options: " + JSON.stringify(qs))
 	request({
 		uri: 'http://api.uinames.com',
-		json: true
+		json: true,
+		qs: qs
 	}, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			res.end(body.name + " " + body.surname);
+			if (body.error) {
+				if (body.error === "No matching pool found") {
+					res.send("The country '" + qs.country + "' couldn't be found.")
+				} else {
+					res.send("There was an error talking to uinames.com (" + body.error + ")")
+				}
+			} else {
+				if (Array.isArray(body)) {
+					res.send(body.map(function(person) {
+						return formatPerson(person)
+					}).join("\n"))
+				} else {
+					res.send(formatPerson(body))
+				}
+			}
 		}
 	})
-});
+})
 
-app.listen(port);
-console.log('Server running at http://' + host + ':' + port + '/');
+function formatPerson(person) {
+	return person.name + " " + person.surname
+}
+
+app.listen(port)
+console.log('Server running at http://' + host + ':' + port + '/')
